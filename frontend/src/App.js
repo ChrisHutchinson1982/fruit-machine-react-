b31a94308f925c42d76b5ba8f3f5ec963283663c
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-16 21:34:44 +0000

    Refactored app into separtte components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,16 @@
 import "./App.css";
+import SpinRow from "./components/SpinRow";
+import SpinComment from "./components/SpinComment";
 import { useState } from "react";
 
-// JSX - Jaascript Extended
-// Import concepts
-// - Props
-// - State
-
 function App() {
-  const [spinOne, setSpinOne] = useState("❓");
-  const [spinTwo, setSpinTwo] = useState("❓");
-  const [spinThree, setSpinThree] = useState("❓");
-
-  const changeSpin = () => {
-    setSpinOne("🍒");
-    setSpinTwo("🍒");
-    setSpinThree("🍒");
-  };
+  const [spinComment, setSpinComment] = useState("");
 
   return (
     <div className="App">
       <h1>Fruit Machine</h1>
-      <div className="spin-row">
-        <h2 className="spin-one">{spinOne}</h2>
-        <h2 className="spin-two">{spinTwo}</h2>
-        <h2 className="spin-three">{spinThree}</h2>
-      </div>
-      <div className="spin-button">
-        <button onClick={changeSpin}>Hit to spin</button>
-      </div>
-      <div className="comment-container">
-        <h2 className="comment">Spin the wheel, it might be your lucky day!</h2>
-      </div>
+      <SpinRow setSpinComment={setSpinComment} />
+      <SpinComment spinComment={spinComment} />
     </div>
   );
 }
