4fd168e0cab1a7eb74884614ae669fcb43aad7f6
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-02-18 18:45:26 +0000

    Passed feature test that the game shows 3 random fruit emojis when button is clicked

--- frontend/cypress/e2e/user_plays_game.cy.js
+++ frontend/cypress/e2e/user_plays_game.cy.js
@@ -1,8 +1,24 @@
 describe("User plays game", () => {
-  it("start the game with 3 questions marks", () => {
+  it("game starts with 3 questions marks", () => {
     cy.visit("http://localhost:3000/");
     cy.get('[data-cy="spin1"]').should("contain.text", "❓");
     cy.get('[data-cy="spin2"]').should("contain.text", "❓");
     cy.get('[data-cy="spin3"]').should("contain.text", "❓");
+  });
+  it("game shows 3 random fruit emojis when button is selected", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="spin-button"]').click();
+
+    cy.get('[data-cy="spin1"]').each((x) => {
+      expect(x.text()).to.be.oneOf(["🍒", "🍌", "🍏", "🍊", "🍇"]);
+    });
+
+    cy.get('[data-cy="spin2"]').each((x) => {
+      expect(x.text()).to.be.oneOf(["🍒", "🍌", "🍏", "🍊", "🍇"]);
+    });
+
+    cy.get('[data-cy="spin3"]').each((x) => {
+      expect(x.text()).to.be.oneOf(["🍒", "🍌", "🍏", "🍊", "🍇"]);
+    });
   });
 });
